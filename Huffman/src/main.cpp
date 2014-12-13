5f05650d5a7a162ec88784952262c9b014a9ccea
EmiPatoR <emipator@gmail.com>
2014-12-13 12:29:45 +0100

    Init commit

---
+++ Huffman/src/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : HRUSTIC Emir
+// Version     :
+// Copyright   : 
+// Description : Huffman coding in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello world !" << endl; // prints
+	return 0;
+}
